ca6af82449d59036ee30cdf4cec4ed024bd4853e
Pavel Naumovets <pavel.naumovets@mail.ru>
2021-07-29 22:49:31 +0300

    Answer

--- src/main/java/com/epam/rd/autotasks/Sorting.java
+++ src/main/java/com/epam/rd/autotasks/Sorting.java
@@ -1,7 +1,22 @@
 package com.epam.rd.autotasks;
 
+import java.nio.Buffer;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class Sorting {
     public void sort(int[] array){
-        throw new UnsupportedOperationException();
+        if(array == null){
+            throw new IllegalArgumentException();
+        } else{
+            Arrays.sort(array);
+        }
+    }
+
+    public static void main(String[] args) {
+        Sorting sorting = new Sorting();
+        int[] init = null;
+        sorting.sort(init);
+        System.out.println(Arrays.toString(init));
     }
 }
--- src/main/java/com/epam/rd/autotasks/SortingTest.java
+++ src/main/java/com/epam/rd/autotasks/SortingTest.java
@@ -1,5 +1,6 @@
 package com.epam.rd.autotasks;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 
@@ -7,29 +8,45 @@ public class SortingTest {
 
     Sorting sorting = new Sorting();
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testNullCase(){
-
+        int[] array_1 = null;
+        int[] array_2 = null;
+        sorting.sort(array_2);
+        Assert.assertArrayEquals(array_1, array_2);
     }
 
     @Test
     public void testEmptyCase(){
-
+        int[] array_1 = new int[]{};
+        int[] array_2 = new int[]{};
+        sorting.sort(array_2);
+        Assert.assertArrayEquals(array_1,array_2);
     }
 
     @Test
     public void testSingleElementArrayCase() {
-
+        int[] array_1 = new int[]{1};
+        int[] array_2 = new int[]{1};
+        sorting.sort(array_2);
+        Assert.assertArrayEquals(array_1,array_2);
     }
 
     @Test
     public void testSortedArraysCase() {
+        int[] array_1 = new int[]{1,2,3,4,5};
+        int[] array_2 = new int[]{1,2,3,4,5};
+        sorting.sort(array_2);
+        Assert.assertArrayEquals(array_1,array_2);
 
     }
 
     @Test
     public void testOtherCases() {
-
+        int[] array_1 = new int[]{1,2,3,4,5,6,9,8,7,10};
+        int[] array_2 = new int[]{1,2,3,4,5,6,7,8,9,10};
+        sorting.sort(array_1);
+        Assert.assertArrayEquals(array_2,array_1);
     }
 
 }
